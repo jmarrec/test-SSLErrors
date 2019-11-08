ad02a60bfbb69a5b88bce546dde6db68d137e187
Julien Marrec <julien.marrec@gmail.com>
2019-11-08 13:21:37 +0100

    Here's the fix using config.set_validate_certificates(false)

--- main.cpp
+++ main.cpp
@@ -11,28 +11,38 @@
 #include <sstream>
 #include <regex>
 
+
 int main(int argc, char * argv[]) {
 
-  // If I change this to http, it works
-  auto ip = web::http::client::http_client(U("https://checkip.amazonaws.com/"))
-    .request(web::http::methods::GET)
-    .then([](web::http::http_response response) {
+  web::http::client::http_client_config config;
+  config.set_validate_certificates(false);
+
+  // Note: If I change this to http, it works without setting validate_certificates(false)
+  auto ip = web::http::client::http_client(U("https://checkip.amazonaws.com/"), config)
+  .request(web::http::methods::GET)
+  .then([](web::http::http_response response) {
       auto statusCode = response.status_code();
-      std::cout << statusCode << std::endl;
       if (statusCode != 200) {
         std::stringstream ss;
         ss << "Error: response code was " << statusCode;
+        std::cout << ss.str() << '\n';
         return ss.str();
       }
 
       auto body = response.extract_utf8string(true).get();
+
       // Remove trailing line ending
       return body.erase(body.find_last_not_of("\n") + 1);
     })
-    .get();
+  .get();
 
   std::regex ipRegex("^\\d{1,3}(?:\\.\\d{1,3}){3}$");
-  std::cout << "isOnline = " << std::boolalpha << std::regex_search(ip, ipRegex);
+  std::smatch ipMatch;
+  bool isOnline = std::regex_search(ip, ipMatch, ipRegex);
+  std::cout << "isOnline = " << std::boolalpha << isOnline << '\n';
+  if (isOnline) {
+    std::cout << "IP=" << ipMatch[0] << '\n';
+  }
 
   return 0;
 }
